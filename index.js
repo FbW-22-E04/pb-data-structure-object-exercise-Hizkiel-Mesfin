85081b35c9f8c4d998b2b3d823aa5924930a942b
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-08-10 14:56:21 +0200

    corrections

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const favRecipe = {
   ingredients: ["onion", "garlic", "ground meat", "lasagna pasta", "olive oil"],
 };
 
+console.log(favRecipe.title);
+console.log(Object.entries(favRecipe)[1].join(" "));
+console.log(Object.keys(favRecipe)[2] + ":");
+
+for (const ing of favRecipe.ingredients) {
+  console.log(ing);
+}
+
 //Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
 //Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 //Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.
